a53fa619e1cd03be4b2336470382211defa94c37
Dimitrios Pritsos <dpritsos@extremepro.gr>
2012-01-31 21:10:41 +0200

    Forgot these too sorry about that

--- src/html2tf/dictionaries/cngrams.py
+++ src/html2tf/dictionaries/cngrams.py
@@ -1,54 +1,6 @@
 """ """
 
 from htmlattrib.attrib import HtmlText
-
-class BaseString2NgramList(object):
-    
-    def __init__(self, n):
-        self.n = n
-        #self.ngrms_l = list()
-    
-    def terms_lst(self, text):
-        if not text:
-            return None
-        ngrms_l =  list()
-        for i in range( len(text) - self.n + 1 ): 
-            ngrms_l.append( text[i : i+self.n]  )
-        #ngrms_l.sort() #it is nice to be sorted and to conform with numpy.unique1d results   
-        return ngrms_l
-    
-    
-class BaseString2TFTP(BaseString2NgramList):
-    
-    def __init__(self, n):
-        BaseString2NgramList.__init__(self, n)
-    
-    def nf_dict(self, text):
-        if not text:
-            return None
-        #Create NGram List if has been not previously generated
-        ngrms_l = self.terms_lst(text)
-        #Count NGrams and Build the Ngram-Frequency (TF) dictionary 
-        NgF_d = dict()
-        for ng in ngrms_l:
-            if ng in NgF_d: #if the dictionary of terms has the 'terms' as a key 
-                NgF_d[ ng ] += 1
-            else: 
-                NgF_d[ ng ] = 1 
-        return NgF_d  
-    
-    def npos_dict(self, text):
-        if not text:
-            return None
-        #Create NGram List if has been not previously generated    
-        ngrms_l = self.terms_lst(text)
-        NgPos_d = dict()
-        for i, ng in enumerate(ngrms_l):
-            if ng in NgPos_d: #if the dictionary of terms has the 'terms' as a key 
-                NgPos_d[ ng ].append(i)
-            else: 
-                NgPos_d[ ng ] = [ i ] 
-        return NgPos_d
   
 
 class Html2TF(BaseString2TFTP, HtmlText):
--- src/html2tf/tables/cngrams.py
+++ src/html2tf/tables/cngrams.py
@@ -1,3 +1,8 @@
+#
+#
+#
+#
+
 """ """
 
 from htmlattrib.attrib import HtmlText
@@ -5,42 +10,6 @@ from html2tf.dictionaries.cngrams import BaseString2NgramList
 import tbtools
 import numpy as np
 
-class BaseString2TFTP(BaseString2NgramList):
-    
-    def __init__(self, n):
-        BaseString2NgramList.__init__(self, n)
-            
-    def tf_array(self, text, ndtype=tbtools.default_TF_3grams_dtype):
-        
-        if not text:
-            return None
-        
-        #Find NGrams
-        ngrms_l = self.terms_lst(text)
-        
-        #Count NGrams
-        terms, inds = np.unique1d(ngrms_l, return_inverse=True)
-        freqs = np.bincount(inds)
-        
-        #We need a Recored arrays to be created thus using numpy.rec.array.fromarrays()
-        #to be invoked as follows (alternatively fromarrays() can be used directly   
-        #print terms
-        NgF_arr = np.rec.array([terms, freqs], dtype=ndtype)
-        return NgF_arr
-    
-    def tpos_array(self, text, ndtype=tbtools.default_TP_3grams_dtype):
-        if not text:
-            return None
-        #Find and Count NGrams
-        ngrms_l = self.terms_lst(text)
-        terms, inds = np.unique1d(ngrms_l, return_inverse=True)
-        NgP_arr = np.zeros(len(terms) , dtype=ndtype)
-        NgP_arr['terms'] = terms
-        pos_arr_arr =[np.where(inds == idx) for idx in np.arange(len(terms))]
-        for i, pos_arr in enumerate(pos_arr_arr):
-            NgP_arr['pos'][i][0:len(pos_arr[0])] = pos_arr[0] + 1
-        return NgP_arr
-    
 
 class Html2TF(BaseString2TFTP, HtmlText):
     
