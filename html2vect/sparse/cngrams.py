d83f541735bad0393593b0ecf17d267c6934d2a5
Dimitrios Pritsos <dpritsos@extremepro.gr>
2015-10-18 21:07:53 +0300

    tid_dictionary to tid_vocabulary variable name will gradually changes in all files in the package.

--- html2vect/sparse/cngrams.py
+++ html2vect/sparse/cngrams.py
@@ -1,76 +1,74 @@
 #
 #    Module: Character NGrams -TF - from html row text/files to scipy.sparse.csr_matrix Character NGrams-TF
-# 
-#    Author: Dimitiros Pritsos 
-#    
+#
+#    Author: Dimitiros Pritsos
+#
 #    License: BSD Style
 #
-#    Last update: Please refer to the GIT tracking 
-# 
+#    Last update: Please refer to the GIT tracking
+#
 
 """ html2vect.sparse.cngrams: submodule of `html2vect` module defines the classes: Html2TF() """
 
 from ..base.html2tf import BaseHtml2TF
 from ..base.termstypes.cngrams import String2CNGramsList
- 
+
 import scipy.sparse as ssp
 import numpy as np
 
 
 
 class Html2TF(BaseHtml2TF):
-    
-    #Define the TermsType to be produced from this class 
+
+    #Define the TermsType to be produced from this class
     s2ngl = String2CNGramsList()
-    
-       
+
+
     def __init__(self, *args, **kwrgs):
-            
-        #Initialise BaseHtml2TF Class   
+
+        #Initialise BaseHtml2TF Class
         super(Html2TF, self).__init__(*args, **kwrgs)
-            
-        
-    def yield_(self, xhtml_file_l, tid_dictionary, norm_func, encoding, error_handling):
-        
+
+
+    def yield_(self, xhtml_file_l, tid_vocabulary, norm_func, encoding, error_handling):
+
         #Create the Dictionary from the given corpus if not given form the use
-        if tid_dictionary == None:
-            tid_dictionary = self.__build_vocabulery(xhtml_file_l, encoding, error_handling)
-            
+        if tid_vocabulary == None:
+            tid_vocabulary = self.__build_vocabulery(xhtml_file_l, encoding, error_handling)
+
         print "Creating NGrams-TF"
         #Create the NGrams-TF Sparse Matrix for the whole corpus
         fq_lst = list()
         for html_str in self.load_files(xhtml_file_l, encoding, error_handling):
-            fq_lst.append( self.tl2tf.trms2f_sparse( Html2TF.s2ngl.terms_lst( self.html_attrib( html_str ) ), tid_dictionary, norm_func) )
-        
+            fq_lst.append( self.tl2tf.trms2f_sparse( Html2TF.s2ngl.terms_lst( self.html_attrib( html_str ) ), tid_vocabulary, norm_func) )
+
         #Pack it as a sparse vstack and return it
         copus_fq_array = ssp.vstack( fq_lst )
-        return ( ssp.csr_matrix(copus_fq_array, shape=copus_fq_array.shape, dtype=np.float32), tid_dictionary )
-    
-    
+        return ( ssp.csr_matrix(copus_fq_array, shape=copus_fq_array.shape, dtype=np.float32), tid_vocabulary )
+
+
     def from_src(self, xhtml_str):
         raise Exception("Please use from_files() or from_paths() methods instead")
-    
-    def from_files(self, xhtml_file_l, tid_dictionary=None, norm_func=None, encoding='utf8', error_handling='strict'):
-        return self.yield_(xhtml_file_l, tid_dictionary, norm_func, encoding, error_handling)  
-    
-        
-    def from_paths(self, basepath, filepath_l, tid_dictionary=None, norm_func=None, encoding='utf8', error_handling='strict'):
-        
-        #Get the filenames located in the paths given 
+
+    def from_files(self, xhtml_file_l, tid_vocabulary=None, norm_func=None, encoding='utf8', error_handling='strict'):
+        return self.yield_(xhtml_file_l, tid_vocabulary, norm_func, encoding, error_handling)  
+
+
+    def from_paths(self, basepath, filepath_l, tid_vocabulary=None, norm_func=None, encoding='utf8', error_handling='strict'):
+
+        #Get the filenames located in the paths given
         xhtml_file_l = self.file_list_frmpaths(basepath, filepath_l)
-        
+
         #Create the vectors sparse matrix for this files
-        matrix, tid_dict = self.from_files(xhtml_file_l, tid_dictionary, norm_func, encoding, error_handling)
-        
+        matrix, tid_dict = self.from_files(xhtml_file_l, tid_vocabulary, norm_func, encoding, error_handling)
+
         #Return the matrix, the dictionary created and the xhtml_files_list
         return (matrix, tid_dict, xhtml_file_l)
-    
-    
-    
-#To Be Written    
+
+
+
+#To Be Written
 class Html2TPL(object):
-    
+
     def __init__(self):
         pass
-        
-    
