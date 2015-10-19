2eabe1f68ad95d16d1ed7417cb28c23f814b6e5f
Dimitrios Pritsos <dpritsos@extremepro.gr>
2015-10-19 17:17:28 +0300

    narrays.cngrams and narrays.wngrams has been implemented.

---
+++ html2vect/narray/wngrams.py
@@ -0,0 +1,34 @@
+#
+#    Module: Words-TF - from html row text/files to scipy.sparse.csr_matrix Words-TF
+# 
+#    Author: Dimitiros Pritsos 
+#    
+#    License: BSD Style
+#
+#    Last update: Please refer to the GIT tracking 
+# 
+
+""" html2vect.sparse.wngrams: submodule of `html2vect` module defines the classes: Html2TF() """
+
+from .cngrams import Html2TF as CHtml2TF
+from ..base.termstypes.wngrams import String2WNGramsList
+
+
+
+class Html2TF(CHtml2TF):
+    
+    #Define the TermsType to be produced from this class 
+    s2ngl = String2WNGramsList()
+    
+    
+    def __init__(self, *args, **kwrgs):
+            
+        #Initialise BaseHtml2TF Class   
+        super(Html2TF, self).__init__(*args, **kwrgs)
+               
+        
+#To Be Written    
+class Html2TPL(object):
+    
+    def __init__(self):
+        pass
